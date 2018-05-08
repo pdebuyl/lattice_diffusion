7c423a7f6b47ffb651aea71902d24563a743eab6
Pierre de Buyl <pdebuyl@pdebuyl.be>
2018-05-08 22:21:54 +0200

    add velocity shifted density

--- scripts/show_diff.py
+++ scripts/show_diff.py
@@ -10,6 +10,7 @@ parser.add_argument('--show-n', action='store_true')
 parser.add_argument('--show-d', action='store_true')
 parser.add_argument('--show-rho0', action='store_true')
 parser.add_argument('--stride', default=1, type=int)
+parser.add_argument('--no-plot', action='store_false')
 args = parser.parse_args()
 
 conf = {}
@@ -24,7 +25,7 @@ with open(args.config_file, 'r') as f:
 
 p_flip = float(conf['p_flip'])
 p_drive = float(conf['p_drive'])
-p_move = float(conf['p_move'])
+dt = float(conf['dt'])
 n_inner = int(conf['n_inner'])
 n_particles = int(conf['n_particles'])
 rho_0 = int(conf['rho_0'])
@@ -35,7 +36,7 @@ cst_bc = conf['cst_bc'] == 'T'
 
 D = 0
 if do_diffusion:
-    D = D + 0.5*p_move
+    D = D + 1
 if do_drive:
     D = D + (0.5 + 1/(np.exp(2*p_flip)-1))*p_drive
 
@@ -59,19 +60,26 @@ def diffusion(x, x0, D, t):
     res = norm_factor * np.exp(-(x-x0)**2/(4*D*t))
     return res
 
+if not args.show_d:
+    ax1 = plt.subplot(211)
+    ax2 = plt.subplot(212)
+else:
+    ax1 = plt.subplot(111)
+
 for i, step in enumerate(m[::args.stride]):
-    l, = plt.plot(xr, step)
+    t = n_inner * dt * (1+i*args.stride)
+
+    l, = ax1.plot(xr, step)
 
     if args.show_d:
-        t = n_inner * (1+i*args.stride)
-        plt.plot(xr, n_particles*diffusion(xr, n_bins//2-1/2, D, t), ls='--', color=l.get_color())
+        ax1.plot(xr, n_particles*diffusion(xr, n_bins//2-1, D, t), ls='--', color=l.get_color())
 
 if args.show_rho0:
     plt.axhline(rho_0)
 
 if args.show_n:
     n = m.sum(axis=1)
-    t = (np.arange(len(n))+1)*n_inner
+    t = (np.arange(len(n)))*n_inner*dt
     plt.figure()
     plt.plot(t, n)
     fit = np.polyfit(t[len(n)//2:], n[len(n)//2:], 1)
@@ -80,6 +88,11 @@ if args.show_n:
     if not cst_bc:
         vel /= 2
     print("Front velocity          :", vel)
-    print("Front velocity / sqrt(2):", vel/np.sqrt(2))
 
-plt.show()
+if not args.show_d:
+    for i, step in enumerate(m[::args.stride]):
+        t = n_inner * dt * (1+i*args.stride)
+        ax2.plot(xr - vel*t, step)
+
+if args.no_plot:
+    plt.show()
